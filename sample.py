85dee94ff2a89d3c6ccbaf4c76b3acf65e4a5313
Sudarshan <86884712+Sudarshan-TN@users.noreply.github.com>
2023-12-05 14:49:38 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,92 +1,9 @@
-import os
-
-import boto.utils
-import boto3
 import requests
-import datetime
-import time
 
-
-def get_contents(filename):
-    with open(filename) as f:
-        return f.read()
-
-
-def get_ecs_introspection_url(resource):
-    # 172.17.0.1 is the docker network bridge ip
-    return 'http://172.17.0.1:51678/v1/' + resource
-
-
-def contains_key(d, key):
-    return key in d and d[key] is not None
-
-
-def get_local_container_info():
-    # get the docker container id
-    # http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-introspection.html
-    docker_id = os.path.basename(get_contents("/proc/1/cpuset")).strip()
-    print('----------------docker_id ',docker_id)
-    if docker_id is None:
-        raise Exception("Unable to find docker id")
-
-    ecs_local_task = requests.get(get_ecs_introspection_url('tasks') + '?dockerid=' + docker_id).json()
-    print('------------------ecs_local_task ',ecs_local_task)
-    task_arn = ecs_local_task['Arn']
-
-    if task_arn is None:
-        raise Exception("Unable to find task arn for container %s in ecs introspection api" % docker_id)
-
-    ecs_local_container = None
-
-    if contains_key(ecs_local_task, 'Containers'):
-        for c in ecs_local_task['Containers']:
-            if c['DockerId'] == docker_id:
-                ecs_local_container = c
-
-    if ecs_local_container is None:
-        raise Exception("Unable to find container %s in ecs introspection api" % docker_id)
-
-    return ecs_local_container['Name'], task_arn
-
-
-def main():
-
-    region = os.environ["AWS_REGION"] # last char is the zone, which we don't care about
-    cluster = os.environ["CLUSTER"]
-
-    container_name, task_arn = get_local_container_info()
-
-    # Get the container info from ECS. This will give us the port mappings
-    ecs = boto3.client('ecs', region_name=region)
-    response = ecs.describe_tasks(
-        cluster=cluster,
-        tasks=[
-            task_arn,
-        ]
-    )
-    print('-----------------ECS Describe: ', response)
-    task = None
-    if contains_key(response, 'tasks'):
-        for t in response['tasks']:
-            if t['taskArn'] == task_arn:
-                task = t
-
-    if task is None:
-        raise Exception("Unable to locate task %s" % task_arn)
-
-    container = None
-    if contains_key(task, 'containers'):
-        for c in task['containers']:
-            if c['name'] == container_name:
-                container = c
-
-    if container is None:
-        raise Exception("Unable to find ecs container %s" % container_name)
-
-    if contains_key(container, 'networkBindings'):
-        for b in container['networkBindings']:
-            print("export PORT_%s_%d=%d;" % (b['protocol'].upper(), b['containerPort'], b['hostPort']))
-
-
-if __name__ == '__main__':
-    main()
+try:
+    response = requests.get('http://localhost:51678/v1/metadata')
+    response.raise_for_status()
+    metadata = response.json()
+    print(metadata)
+except requests.exceptions.RequestException as e:
+    print(f'Failed to retrieve ECS container metadata: {e}')
